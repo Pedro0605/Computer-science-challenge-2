76ded65121c3a737dc3de8cde8b1f82063c32587
Pedro0605 <80006617+Pedro0605@users.noreply.github.com>
2022-06-23 13:31:55 +0000

    finish

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
@@ -6,4 +8,6 @@ document.querySelector("form").addEventListener("submit", function(e){
     localStorage.setItem("name", name);
     localStorage.setItem("email", email);
     localStorage.setItem("password", password);
+
+    document.querySelector("form").reset();
 });
