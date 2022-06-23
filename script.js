c57bf481ae6f2921584d1ef751238d005cc61b27
Pedro0605 <80006617+Pedro0605@users.noreply.github.com>
2022-06-23 12:27:26 +0000

    done!

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 document.querySelector("form").addEventListener("submit", function(e){
-    console.log("send");
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-
-    console.log(name, email, password);
 
     localStorage.setItem("name", name);
     localStorage.setItem("email", email);
